6e58322afec49c2a4f0c3eaa7decaa163c2bff18
Amodh Naik <AkMa11@users.noreply.github.com>
2018-08-16 15:56:47 +0530

    added contact page with info

--- script/script.js
+++ script/script.js
@@ -4,3 +4,11 @@ hamburger.addEventListener("click", function() {
   navDisplay = document.querySelector(".nav_display");
   navDisplay.classList.toggle("nav_display_grid");
 });
+
+const directions = document.querySelector(".maps");
+directions.addEventListener("click", function() {
+  window.open(
+    "http://maps.google.com/maps?saddr=My+Location&daddr=19.0513933,73.0728258",
+    "_blank"
+  );
+});
